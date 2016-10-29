26d0e741fa356711986a6f03b8ec6f7043e1570a
Andrew L. Van Slaars <avanslaars@gmail.com>
2016-10-29 13:05:04 -0400

    Initial commit of lesson code

---
+++ README.md
@@ -0,0 +1,9 @@
+## Running the code:
+
+* Be sure you are running a recent version of node & npm (in the video, I'm running node 6.9.1 and npm 3.10.8)
+* Install next by running `npm install`
+
+## Deploying the code
+
+* install the latest version of now with `npm install -g now`
+* If you don't have an account, you will be promted for an email address to create an auth token
---
+++ pages/contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+export default class extends React.Component {
+  static getInitialProps({req}) {
+    const renderLocation = req ? 'Server' : 'Client'
+    return {
+      renderLocation
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contact Page</h1>
+        <Nav />
+        <p>
+          This page was rendered
+          on the {this.props.renderLocation}
+        </p>
+      </div>
+    )
+  }
+}
